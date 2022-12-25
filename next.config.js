1e8225a33672fba1222f4854da8629d33353939b
muditvir <mvirmani02@gmail.com>
2022-12-26 03:12:07 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    newNextLinkBehavior: false,
+  },
+  images:{
+    domains:['bayut-production.s3.eu-central-1.amazonaws.com']
+  }
 }
 
 module.exports = nextConfig
